ffa5573c79dbee005ae04fa9721847aba6fdfe7f
BuildTools <unconfigured@null.spigotmc.org>
2020-05-27 11:15:46 -0700

    WorldGuard v6 and v7 compatibility

--- MechanicsCoreCompatibility/WorldGuardV7/src/me/deecaad/compatibility/worldguard/WorldGuardV7.java
+++ MechanicsCoreCompatibility/WorldGuardV7/src/me/deecaad/compatibility/worldguard/WorldGuardV7.java
@@ -5,22 +5,32 @@ import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.flags.DoubleFlag;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.IntegerFlag;
 import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.StringFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
-import io.netty.handler.logging.LogLevel;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import javax.annotation.Nullable;
+import java.util.HashMap;
 import java.util.Map;
+
+import static me.deecaad.core.MechanicsCore.debug;
 
 public class WorldGuardV7 implements IWorldGuardCompatibility {
 
-    private Map<String, StateFlag> flags;
+    private Map<String, Flag<?>> flags;
+    private FlagRegistry registry;
 
     public WorldGuardV7() {
-
+        flags = new HashMap<>();
+        registry = WorldGuard.getInstance().getFlagRegistry();
     }
 
     @Override
@@ -30,18 +40,75 @@ public class WorldGuardV7 implements IWorldGuardCompatibility {
         ApplicableRegionSet applicableRegionSet = regionQuery.getApplicableRegions(BukkitAdapter.adapt(location));
         LocalPlayer local = player == null ? null : WorldGuardPlugin.inst().wrapPlayer(player);
 
-        StateFlag flag = flags.get(flagName);
+        Flag<?> flag = flags.get(flagName);
         if (flag == null) {
             String flagList = "[" + String.join(", ", flags.keySet()) + "]";
-            //debug.log(LogLevel.ERROR, "Flag \"" + flagName + "\" does not exist...", "Available flags: " + flagList);
+            debug.error("Flag \"" + flagName + "\" does not exist...", "Available flags: " + flagList);
+            return true;
+
+        } else if (!(flag instanceof StateFlag)) {
+            debug.error("Flag \"" + flagName + "\" is not a StateFlag!!!");
+            return true;
         }
 
-        return false;
+        StateFlag stateFlag = (StateFlag) flag;
+        return applicableRegionSet.testState(local, stateFlag);
     }
 
     @Override
-    public void registerFlags(String... flags) {
+    public Object getValue(Location location, String flagName) {
+        RegionContainer regionContainer = WorldGuard.getInstance().getPlatform().getRegionContainer();
+        RegionQuery regionQuery = regionContainer.createQuery();
+        ApplicableRegionSet applicableRegionSet = regionQuery.getApplicableRegions(BukkitAdapter.adapt(location));
 
+        Flag<?> flag = flags.get(flagName);
+        if (flag == null) {
+            String flagList = "[" + String.join(", ", flags.keySet()) + "]";
+            debug.error("Flag \"" + flagName + "\" does not exist...", "Available flags: " + flagList);
+            return true;
+
+        }
+
+        return applicableRegionSet.queryValue(null, flag);
+    }
+
+    @Override
+    public void registerFlag(String flagString, FlagType type) {
+        Flag<?> flag;
+
+        // An illegal argument exception will occur here if the flag
+        // name is invalid. A valid flag matches: "^[:A-Za-z0-9\\-]{1,40}$"
+        switch (type) {
+            case INT_FLAG:
+                flag = new IntegerFlag(flagString);
+                break;
+            case STATE_FLAG:
+                flag = new StateFlag(flagString, false);
+                break;
+            case DOUBLE_FLAG:
+                flag = new DoubleFlag(flagString);
+                break;
+            case STRING_FLAG:
+                flag = new StringFlag(flagString);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown FlagType " + type);
+        }
+
+        try {
+            registry.register(flag);
+        } catch (FlagConflictException ex) {
+            Flag<?> existing = registry.get(flagString);
+
+            if (existing.getClass() == flag.getClass()) {
+                debug.warn("Flag " + flagString + " already registered. This may cause compatibility issues!");
+                flag = existing;
+            } else {
+                debug.error("Incompatible flag types! " + flagString + " is already registered!!!");
+            }
+        }
+
+        flags.put(flagString, flag);
     }
 
     @Override
--- MechanicsCoreCompatibility/src/me/deecaad/compatibility/worldguard/IWorldGuardCompatibility.java
+++ MechanicsCoreCompatibility/src/me/deecaad/compatibility/worldguard/IWorldGuardCompatibility.java
@@ -7,11 +7,47 @@ import javax.annotation.Nullable;
 
 public interface IWorldGuardCompatibility {
 
-    // todo: add docs
+    /**
+     * Gets the <code>StateFlag</code> based on the given flagName
+     * and tests it against the given player at the region found at
+     * the given <code>Location</code>
+     *
+     * @param location Where to test
+     * @param player Who to test against (Ops have more permission, for example)
+     * @param flagName The name of the flag to test
+     * @return true if the flag is enabled in the region
+     */
     boolean testFlag(Location location, @Nullable Player player, String flagName);
 
-    // todo: add docs
-    void registerFlags(String... flags);
+    /**
+     * Gets the value of the <code>Flag</code> with the given flagName
+     * in the region found at the given <code>Location</code>
+     *
+     * @param location Where to get the value
+     * @param flagName The name of the Flag to use
+     * @return The value of the flag
+     */
+    Object getValue(Location location, String flagName);
 
+    /**
+     * Registers a< <code>Flag</code> with the given name and type
+     *
+     * @param flag The name of the flag
+     * @param type Which type the flag should be
+     */
+    void registerFlag(String flag, FlagType type);
+
+    /**
+     * @return true if WorldGuard is installed
+     */
     boolean isInstalled();
+
+    public static enum FlagType {
+
+        STATE_FLAG,
+        DOUBLE_FLAG,
+        INT_FLAG,
+        STRING_FLAG,
+
+    }
 }
