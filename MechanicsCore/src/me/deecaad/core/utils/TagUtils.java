4e5863298ae1bc57450152b05b12cf573a3851da
BuildTools <unconfigured@null.spigotmc.org>
2020-06-24 01:11:31 +0300

    Updated TagHelper and TagUtils

--- MechanicsCore/src/me/deecaad/core/utils/TagUtils.java
+++ MechanicsCore/src/me/deecaad/core/utils/TagUtils.java
@@ -3,15 +3,13 @@ package me.deecaad.core.utils;
 import me.deecaad.compatibility.CompatibilityAPI;
 import me.deecaad.core.MechanicsCore;
 import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.tags.CustomItemTagContainer;
 import org.bukkit.inventory.meta.tags.ItemTagType;
-import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 /**
  * Class to easily get and set different data types
@@ -22,91 +20,168 @@ public class TagUtils {
     /**
      * Don't let anyone instantiate this class
      */
-    private TagUtils() {
-    }
+    private TagUtils() { }
 
     /**
-     * Gets the <code>Integer</code> stored in the given
-     * NBT tag, "tag" held by the given item. If the item
-     * does not have the tag, this method will return null
-     *
-     * @param item The item storing the tag
-     * @param tag The tag to pull the Integer from
-     * @return The value or null
+     * @param itemStack the item stack from which to get tag
+     * @param tag the tag name
+     * @return the value of the tag (may be null)
      */
-    @Nullable
-    public static Integer getInteger(@Nonnull ItemStack item, @Nonnull String tag) {
-
-        ItemMeta meta = item.getItemMeta();
-        double version = CompatibilityAPI.getVersion();
+    public static String getStringTag(ItemStack itemStack, String tag) {
 
         // 1.14 R1 and above PersistentDataContainer
-        if (version >= 1.141) {
-            PersistentDataContainer keys = meta.getPersistentDataContainer();
-            NamespacedKey key = new NamespacedKey(MechanicsCore.getPlugin(), tag);
+        if (CompatibilityAPI.getVersion() >= 1.141) {
+            ItemMeta itemMeta = itemStack.getItemMeta();
 
-            boolean has = keys.has(key, PersistentDataType.INTEGER);
-            return has ? keys.get(key, PersistentDataType.INTEGER) : null;
+            NamespacedKey key = new NamespacedKey(MechanicsCore.getPlugin(), tag);
+            return itemMeta.getPersistentDataContainer().has(key, PersistentDataType.STRING) ? itemMeta.getPersistentDataContainer().get(key, PersistentDataType.STRING) : null;
         }
 
         // 1.13 R2 CustomItemTagContainer
-        else if (version >= 1.132) {
-            NamespacedKey key = new NamespacedKey(MechanicsCore.getPlugin(), tag);
-            CustomItemTagContainer keys = meta.getCustomTagContainer();
-
-            boolean has = keys.hasCustomTag(key, ItemTagType.INTEGER);
-            return has ? keys.getCustomTag(key, ItemTagType.INTEGER) : null;
-        }
-
-        // 1.13 R1 and lower NMS
-        else {
-            String str = CompatibilityAPI.getCompatibility().getNBTCompatibility().getCustomTag(item, tag);
-
-            // Since NBT compatibility can only get/set
-            // Strings from tags, we have to parse the
-            // String to an Integer. This means that performance
-            // will be better on versions 1.13r2 and higher
-            return (str == null) ? null : Integer.valueOf(str);
-        }
-    }
-
-    /**
-     * Sets the <code>Integer</code> value of the given NBT tag
-     * "tag" held by the given <code>ItemStack</code>. This method
-     * returns the ItemStack with the NBT tag
-     *
-     * @param item The item to set the tag for
-     * @param tag Which tag to set
-     * @param value The value to set for the tag
-     * @return The updated ItemStack with the set value for the tag
-     */
-    public static ItemStack setInteger(ItemStack item, @Nonnull String tag, Integer value) {
-
-        ItemMeta meta = item.getItemMeta();
-        double version = CompatibilityAPI.getVersion();
-
-
-        // 1.14 R1 and above PersistentDataContainer
-        if (version >= 1.141) {
+        if (CompatibilityAPI.getVersion() >= 1.132) {
+            ItemMeta itemMeta = itemStack.getItemMeta();
 
             NamespacedKey key = new NamespacedKey(MechanicsCore.getPlugin(), tag);
-            meta.getPersistentDataContainer().set(key, PersistentDataType.INTEGER, value);
-
-            item.setItemMeta(meta);
-            return item;
-        }
-
-        // 1.13 R2 CustomItemTagContainer
-        if (version >= 1.132) {
-
-            NamespacedKey key = new NamespacedKey(MechanicsCore.getPlugin(), tag);
-            meta.getCustomTagContainer().setCustomTag(key, ItemTagType.INTEGER, value);
-
-            item.setItemMeta(meta);
-            return item;
+            return itemMeta.getCustomTagContainer().hasCustomTag(key, ItemTagType.STRING) ? itemMeta.getCustomTagContainer().getCustomTag(key, ItemTagType.STRING) : null;
         }
 
         // 1.13 R1 and lower full NMS
-        return CompatibilityAPI.getCompatibility().getNBTCompatibility().setCustomTag(item, tag, String.valueOf(value));
+        return CompatibilityAPI.getCompatibility().getNBTCompatibility().getCustomTag(itemStack, tag);
+    }
+
+    /**
+     * Set or change tag of item stack with given value.
+     *
+     * @param itemStack the item stack to modify
+     * @param tag the tag name
+     * @param value the value for tag
+     * @return the item stack with new or modified tag value
+     */
+    public static ItemStack setStringTag(ItemStack itemStack, String tag, String value) {
+
+        // 1.14 R1 and above PersistentDataContainer
+        if (CompatibilityAPI.getVersion() >= 1.141) {
+            ItemMeta itemMeta = itemStack.getItemMeta();
+
+            NamespacedKey key = new NamespacedKey(MechanicsCore.getPlugin(), tag);
+            itemMeta.getPersistentDataContainer().set(key, PersistentDataType.STRING, value);
+
+            itemStack.setItemMeta(itemMeta);
+            return itemStack;
+        }
+
+        // 1.13 R2 CustomItemTagContainer
+        if (CompatibilityAPI.getVersion() >= 1.132) {
+            ItemMeta itemMeta = itemStack.getItemMeta();
+
+            NamespacedKey key = new NamespacedKey(MechanicsCore.getPlugin(), tag);
+            itemMeta.getCustomTagContainer().setCustomTag(key, ItemTagType.STRING, value);
+
+            itemStack.setItemMeta(itemMeta);
+            return itemStack;
+        }
+
+        // 1.13 R1 and lower full NMS
+        return CompatibilityAPI.getCompatibility().getNBTCompatibility().setCustomTag(itemStack, tag, value);
+    }
+
+    /**
+     * @param itemStack the item stack from which to get tag
+     * @param tag the tag name
+     * @return the value of the tag (may be null)
+     */
+    public static Integer getIntegerTag(ItemStack itemStack, String tag) {
+
+        // 1.14 R1 and above PersistentDataContainer
+        if (CompatibilityAPI.getVersion() >= 1.141) {
+            ItemMeta itemMeta = itemStack.getItemMeta();
+
+            NamespacedKey key = new NamespacedKey(MechanicsCore.getPlugin(), tag);
+            return itemMeta.getPersistentDataContainer().has(key, PersistentDataType.INTEGER) ? itemMeta.getPersistentDataContainer().get(key, PersistentDataType.INTEGER) : null;
+        }
+
+        // 1.13 R2 CustomItemTagContainer
+        if (CompatibilityAPI.getVersion() >= 1.132) {
+            ItemMeta itemMeta = itemStack.getItemMeta();
+
+            NamespacedKey key = new NamespacedKey(MechanicsCore.getPlugin(), tag);
+            return itemMeta.getCustomTagContainer().hasCustomTag(key, ItemTagType.INTEGER) ? itemMeta.getCustomTagContainer().getCustomTag(key, ItemTagType.INTEGER) : null;
+        }
+
+        // 1.13 R1 and lower full NMS
+        // Lazy implementation for integers
+        String tagValue = CompatibilityAPI.getCompatibility().getNBTCompatibility().getCustomTag(itemStack, tag);
+        return tagValue == null ? null : Integer.valueOf(tagValue);
+    }
+
+    /**
+     * Set or change tag of item stack with given value.
+     *
+     * @param itemStack the item stack to modify
+     * @param tag the tag name
+     * @param value the value for tag
+     * @return the item stack with new or modified tag value
+     */
+    public static ItemStack setIntegerTag(ItemStack itemStack, String tag, int value) {
+
+        // 1.14 R1 and above PersistentDataContainer
+        if (CompatibilityAPI.getVersion() >= 1.141) {
+            ItemMeta itemMeta = itemStack.getItemMeta();
+
+            NamespacedKey key = new NamespacedKey(MechanicsCore.getPlugin(), tag);
+            itemMeta.getPersistentDataContainer().set(key, PersistentDataType.INTEGER, value);
+
+            itemStack.setItemMeta(itemMeta);
+            return itemStack;
+        }
+
+        // 1.13 R2 CustomItemTagContainer
+        if (CompatibilityAPI.getVersion() >= 1.132) {
+            ItemMeta itemMeta = itemStack.getItemMeta();
+
+            NamespacedKey key = new NamespacedKey(MechanicsCore.getPlugin(), tag);
+            itemMeta.getCustomTagContainer().setCustomTag(key, ItemTagType.INTEGER, value);
+
+            itemStack.setItemMeta(itemMeta);
+            return itemStack;
+        }
+
+        // 1.13 R1 and lower full NMS
+        // Lazy implementation for integers
+        return CompatibilityAPI.getCompatibility().getNBTCompatibility().setCustomTag(itemStack, tag, String.valueOf(value));
+    }
+
+    /**
+     * Adds attribute for item stack with given amount.
+     * This method keeps other attributes set by for example commands or other plugins, BUT
+     * overrides WeaponMechanics old attribute value if found.
+     *
+     * @param itemStack the item stack to which attribute will be set
+     * @param attributeType the attribute type
+     * @param amount the amount for attribute type
+     * @return the item stack with attribute
+     */
+    public static ItemStack setAttributeValue(ItemStack itemStack, AttributeType attributeType, double amount) {
+        if (CompatibilityAPI.getVersion() >= 1.132) { // 1.13 R2 has API for this
+            ItemMeta itemMeta = itemStack.getItemMeta();
+            Attribute attribute = Attribute.valueOf(attributeType.name());
+
+            AttributeModifier hand = new AttributeModifier(attributeType.getMainhandUUID(), "WM_Main", amount, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND);
+            AttributeModifier offhand = new AttributeModifier(attributeType.getOffhandUUID(), "WM_Off", amount, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.OFF_HAND);
+
+            // API doesn't allow modifying AttributeModifiers so I have to delete old ones based on their UUIDs
+            // and add these new AttributeModifiers which contain new amount for the Attribute
+            itemMeta.removeAttributeModifier(attribute, hand);
+            itemMeta.removeAttributeModifier(attribute, offhand);
+
+            itemMeta.addAttributeModifier(attribute, hand);
+            itemMeta.addAttributeModifier(attribute, offhand);
+
+            itemStack.setItemMeta(itemMeta);
+            return itemStack;
+        }
+
+        // 1.13 R1 and lower full NMS
+        return CompatibilityAPI.getCompatibility().getNBTCompatibility().setAttributeValue(itemStack, attributeType, amount);
     }
 }
--- WeaponMechanics/src/me/deecaad/weaponmechanics/utils/TagHelper.java
+++ WeaponMechanics/src/me/deecaad/weaponmechanics/utils/TagHelper.java
@@ -2,6 +2,7 @@ package me.deecaad.weaponmechanics.utils;
 
 import me.deecaad.compatibility.CompatibilityAPI;
 import me.deecaad.core.utils.AttributeType;
+import me.deecaad.core.utils.TagUtils;
 import me.deecaad.weaponmechanics.WeaponMechanics;
 import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
@@ -24,8 +25,8 @@ public class TagHelper {
      * @param tag the tag name
      * @return the value of the tag, if not found null
      */
-    public static String getCustomTag(ItemStack itemStack, CustomTag tag) {
-        return getCustomTag(itemStack, tag.getId());
+    public static String getStringTag(ItemStack itemStack, CustomTag tag) {
+        return TagUtils.getStringTag(itemStack, tag.getId());
     }
 
     /**
@@ -36,35 +37,17 @@ public class TagHelper {
      * @param value the value for tag
      * @return the item stack with new or modified tag value
      */
-    public static ItemStack setCustomTag(ItemStack itemStack, CustomTag tag, String value) {
-        return setCustomTag(itemStack, tag.getId(), value);
+    public static ItemStack setStringTag(ItemStack itemStack, CustomTag tag, String value) {
+        return TagUtils.setStringTag(itemStack, tag.getId(), value);
     }
 
     /**
      * @param itemStack the item stack from which to get tag
      * @param tag the tag name
-     * @return the value of the tag (may be null)
+     * @return the value of the tag, if not found null
      */
-    public static String getCustomTag(ItemStack itemStack, String tag) {
-
-        // 1.14 R1 and above PersistentDataContainer
-        if (CompatibilityAPI.getVersion() >= 1.141) {
-            ItemMeta itemMeta = itemStack.getItemMeta();
-
-            NamespacedKey key = new NamespacedKey(WeaponMechanics.getPlugin(), tag);
-            return itemMeta.getPersistentDataContainer().has(key, PersistentDataType.STRING) ? itemMeta.getPersistentDataContainer().get(key, PersistentDataType.STRING) : null;
-        }
-
-        // 1.13 R2 CustomItemTagContainer
-        if (CompatibilityAPI.getVersion() >= 1.132) {
-            ItemMeta itemMeta = itemStack.getItemMeta();
-
-            NamespacedKey key = new NamespacedKey(WeaponMechanics.getPlugin(), tag);
-            return itemMeta.getCustomTagContainer().hasCustomTag(key, ItemTagType.STRING) ? itemMeta.getCustomTagContainer().getCustomTag(key, ItemTagType.STRING) : null;
-        }
-
-        // 1.13 R1 and lower full NMS
-        return CompatibilityAPI.getCompatibility().getNBTCompatibility().getCustomTag(itemStack, tag);
+    public static Integer getIntegerTag(ItemStack itemStack, CustomTag tag) {
+        return TagUtils.getIntegerTag(itemStack, tag.getId());
     }
 
     /**
@@ -75,65 +58,7 @@ public class TagHelper {
      * @param value the value for tag
      * @return the item stack with new or modified tag value
      */
-    public static ItemStack setCustomTag(ItemStack itemStack, String tag, String value) {
-
-        // 1.14 R1 and above PersistentDataContainer
-        if (CompatibilityAPI.getVersion() >= 1.141) {
-            ItemMeta itemMeta = itemStack.getItemMeta();
-
-            NamespacedKey key = new NamespacedKey(WeaponMechanics.getPlugin(), tag);
-            itemMeta.getPersistentDataContainer().set(key, PersistentDataType.STRING, value);
-
-            itemStack.setItemMeta(itemMeta);
-            return itemStack;
-        }
-
-        // 1.13 R2 CustomItemTagContainer
-        if (CompatibilityAPI.getVersion() >= 1.132) {
-            ItemMeta itemMeta = itemStack.getItemMeta();
-
-            NamespacedKey key = new NamespacedKey(WeaponMechanics.getPlugin(), tag);
-            itemMeta.getCustomTagContainer().setCustomTag(key, ItemTagType.STRING, value);
-
-            itemStack.setItemMeta(itemMeta);
-            return itemStack;
-        }
-
-        // 1.13 R1 and lower full NMS
-        return CompatibilityAPI.getCompatibility().getNBTCompatibility().setCustomTag(itemStack, tag, value);
-    }
-
-    /**
-     * Adds attribute for item stack with given amount.
-     * This method keeps other attributes set by for example commands or other plugins, BUT
-     * overrides WeaponMechanics old attribute value if found.
-     *
-     * @param itemStack the item stack to which attribute will be set
-     * @param attributeType the attribute type
-     * @param amount the amount for attribute type
-     * @return the item stack with attribute
-     */
-    public static ItemStack setAttributeValue(ItemStack itemStack, AttributeType attributeType, double amount) {
-        if (CompatibilityAPI.getVersion() >= 1.132) { // 1.13 R2 has API for this
-            ItemMeta itemMeta = itemStack.getItemMeta();
-            Attribute attribute = Attribute.valueOf(attributeType.name());
-
-            AttributeModifier hand = new AttributeModifier(attributeType.getMainhandUUID(), "WM_Main", amount, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND);
-            AttributeModifier offhand = new AttributeModifier(attributeType.getOffhandUUID(), "WM_Off", amount, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.OFF_HAND);
-
-            // API doesn't allow modifying AttributeModifiers so I have to delete old ones based on their UUIDs
-            // and add these new AttributeModifiers which contain new amount for the Attribute
-            itemMeta.removeAttributeModifier(attribute, hand);
-            itemMeta.removeAttributeModifier(attribute, offhand);
-
-            itemMeta.addAttributeModifier(attribute, hand);
-            itemMeta.addAttributeModifier(attribute, offhand);
-
-            itemStack.setItemMeta(itemMeta);
-            return itemStack;
-        }
-
-        // 1.13 R1 and lower full NMS
-        return CompatibilityAPI.getCompatibility().getNBTCompatibility().setAttributeValue(itemStack, attributeType, amount);
+    public static ItemStack setIntegerTag(ItemStack itemStack, CustomTag tag, int value) {
+        return TagUtils.setIntegerTag(itemStack, tag.getId(), value);
     }
 }
