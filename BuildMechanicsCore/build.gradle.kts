38fd53631d0f2e26f4cd249aa1d8d358449f597f
CJCrafter <collinjbarber@gmail.com>
2021-12-14 05:17:13 -0800

    Auto generated plugin.yml

--- BuildMechanicsCore/build.gradle.kts
+++ BuildMechanicsCore/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("me.deecaad.java-conventions")
     id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
 }
 
 configurations {
@@ -14,8 +17,18 @@ dependencies {
     implementation(project(":WorldGuardV7"))
 }
 
-tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
-    baseName = "MechanicsCore"
+// See https://github.com/Minecrell/plugin-yml
+bukkit {
+    main = "me.deecaad.core.MechanicsCore"
+    name = "MechanicsCore" // Since we don't want to use "BuildMechanicsCore"
+    apiVersion = "1.13"
+
+    authors = listOf("DeeCaaD", "CJCrafter")
+    softDepend = listOf("WorldEdit", "WorldGuard", "PlaceholderAPI")
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    baseName = "MechanicsCore" // Since we don't want to use "BuildMechanicsCore"
     classifier = null;
     configurations = listOf(project.configurations["shadeOnly"], project.configurations["runtimeClasspath"])
 
@@ -31,5 +44,5 @@ tasks.named("assemble").configure {
     dependsOn("shadowJar")
 }
 
-description = "BuildMechanicsCore"
-version = "0.1.0"
+description = "Library plugin for WeaponMechanics"
+version = "0.1.1"
--- BuildWeaponMechanics/build.gradle.kts
+++ BuildWeaponMechanics/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("me.deecaad.java-conventions")
     id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
 }
 
 configurations {
@@ -12,8 +15,18 @@ dependencies {
     implementation(project(":WeaponMechanicsCompatibility"))
 }
 
-tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
-    baseName = "WeaponMechanics"
+// See https://github.com/Minecrell/plugin-yml
+bukkit {
+    main = "me.deecaad.weaponmechanics.WeaponMechanics"
+    name = "WeaponMechanics" // Since we don't want to use "BuildWeaponMechanics"
+    apiVersion = "1.13"
+
+    authors = listOf("DeeCaaD", "CJCrafter")
+    softDepend = listOf("MechanicsCore")
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    baseName = "WeaponMechanics" // Since we don't want to use "BuildWeaponMechanics"
     classifier = null;
     configurations = listOf(project.configurations["shadeOnly"], project.configurations["runtimeClasspath"])
 
@@ -31,5 +44,5 @@ tasks.named("assemble").configure {
     dependsOn("shadowJar")
 }
 
-description = "BuildWeaponMechanics"
-version = "0.2.0"
+description = "A New Age of Weapons in Minecraft"
+version = "0.2.1"
