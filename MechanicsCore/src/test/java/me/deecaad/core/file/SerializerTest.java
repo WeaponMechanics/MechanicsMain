e4ae769cc8020c6f85af64b60d6ab2ec05c99d3d
CJCrafter <collinjbarber@gmail.com>
2022-01-11 18:55:14 -0800

    Quick junit test for serializer

---
+++ MechanicsCore/src/test/java/me/deecaad/core/file/SerializerTest.java
@@ -0,0 +1,47 @@
+package me.deecaad.core.file;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.File;
+
+import static  org.junit.jupiter.api.Assertions.*;
+
+public class SerializerTest {
+
+    private File file;
+    private FileConfiguration config;
+
+    @BeforeEach
+    void setUp() {
+        file = new File("test-config.yml");
+        config = YamlConfiguration.loadConfiguration(file);
+    }
+
+    @AfterEach
+    void tearDown() {
+        file = null;
+        config = null;
+    }
+
+    @Test
+    public void test_parseInvalid() {
+        for (int i = 0; config.contains("Squares.Invalid." + i); i++) {
+            SerializeData data = new SerializeData(new Square(), file, "Squares.Invalid." + i, config);
+
+            assertThrows(SerializerException.class, () ->  data.serializer.serialize(data));
+        }
+    }
+
+    @Test
+    public void test_parseValid() {
+        for (int i = 0; config.contains("Squares.Invalid." + i); i++) {
+            SerializeData data = new SerializeData(new Square(), file, "Squares.Invalid." + i, config);
+
+            assertDoesNotThrow(() ->  data.serializer.serialize(data));
+        }
+    }
+}
---
+++ MechanicsCore/src/test/java/me/deecaad/core/file/Square.java
@@ -0,0 +1,91 @@
+package me.deecaad.core.file;
+
+import javax.annotation.Nonnull;
+
+public class Square implements Serializer<Square> {
+
+    private Vec2 offset;
+    private int length;
+    private double r, g, b;
+
+    /**
+     * Default constructor for serializer
+     */
+    public Square() {
+    }
+
+    public Square(Vec2 offset, int length, double r, double g, double b) {
+        this.offset = offset;
+        this.length = length;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    @Override
+    public String getKeyword() {
+        return "Square";
+    }
+
+    @Nonnull
+    @Override
+    public Square serialize(SerializeData data) throws SerializerException {
+        Vec2 offset = data.of("Offset").serialize(Vec2.class);
+        int length = data.of("Length").assertExists().assertPositive().get();
+        double r = data.of("R").assertRange(0.0, 1.0).get(0.0);
+        double g = data.of("G").assertRange(0.0, 1.0).get(0.0);
+        double b = data.of("B").assertRange(0.0, 1.0).get(0.0);
+
+        if (length == 0)
+            data.throwException("Length", "'Length' may not be '0'");
+
+        return new Square(offset, length, r, g, b);
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "offset=" + offset +
+                ", length=" + length +
+                ", r=" + r +
+                ", g=" + g +
+                ", b=" + b +
+                '}';
+    }
+
+
+    public static class Vec2 implements Serializer<Vec2> {
+
+        private int x, y;
+        private boolean absolute;
+
+        /**
+         * Default constructor for serializer
+         */
+        public Vec2() {
+        }
+
+        public Vec2(int x, int y, boolean absolute) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Nonnull
+        @Override
+        public Vec2 serialize(SerializeData data) throws SerializerException {
+            int x = data.of("X").assertExists().assertType(int.class).get();
+            int y = data.of("Y").assertExists().assertType(Integer.class).get();
+            boolean absolute = data.of("Absolute").assertType(boolean.class).get(true);
+
+            return new Vec2(x, y, absolute);
+        }
+
+        @Override
+        public String toString() {
+            return "Vec2{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+    }
+}
