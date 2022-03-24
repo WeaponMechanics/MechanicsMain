5a90b2b827f4d67ba346a02d8a9df27301f25134
Collin <collinjbarber@gmail.com>
2022-03-24 09:18:49 -0700

    Add mythic mobs drops and armed condition

---
+++ WeaponMechanics/src/main/java/me/deecaad/weaponmechanics/lib/MythicMobsArmedCondition.java
@@ -0,0 +1,54 @@
+package me.deecaad.weaponmechanics.lib;
+
+import io.lumine.mythic.api.adapters.AbstractEntity;
+import io.lumine.mythic.api.config.MythicLineConfig;
+import io.lumine.mythic.api.skills.conditions.IEntityCondition;
+import me.deecaad.weaponmechanics.WeaponMechanics;
+import me.deecaad.weaponmechanics.utils.CustomTag;
+import me.deecaad.weaponmechanics.weapon.info.InfoHandler;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MythicMobsArmedCondition implements IEntityCondition {
+
+    private final boolean wildcard;
+    private final Set<String> weapons;
+
+    public MythicMobsArmedCondition(MythicLineConfig config) {
+        this.weapons = new HashSet<>();
+        String weapons = config.getString(new String[]{ "weapons", "weapon", "title", "weaponTitle", "w"}, "*");
+
+        this.wildcard = "*".equals(weapons.trim());
+        if (wildcard)
+            return;
+
+        InfoHandler info = WeaponMechanics.getWeaponHandler().getInfoHandler();
+        for (String title : weapons.split(", ?")) {
+            this.weapons.add(info.getWeaponTitle(title));
+        }
+    }
+
+    @Override
+    public boolean check(AbstractEntity abstractEntity) {
+        LivingEntity entity = (LivingEntity) abstractEntity.getBukkitEntity();
+
+        EntityEquipment equipment = entity.getEquipment();
+        if (equipment == null)
+            return false;
+
+        ItemStack main = equipment.getItemInMainHand();
+        ItemStack off = equipment.getItemInOffHand();
+
+        String mainTitle = main.hasItemMeta() ? CustomTag.WEAPON_TITLE.getString(main) : null;
+        String offTitle = main.hasItemMeta() ? CustomTag.WEAPON_TITLE.getString(off) : null;
+
+        if (wildcard)
+            return mainTitle != null || offTitle != null;
+        else
+            return (mainTitle != null && weapons.contains(mainTitle)) || (offTitle != null && weapons.contains(offTitle));
+    }
+}
--- WeaponMechanics/src/main/java/me/deecaad/weaponmechanics/lib/MythicMobsLoader.java
+++ WeaponMechanics/src/main/java/me/deecaad/weaponmechanics/lib/MythicMobsLoader.java
@@ -1,5 +1,10 @@
 package me.deecaad.weaponmechanics.lib;
 
+import io.lumine.mythic.apiexamples.conditions.ExampleCondition;
+import io.lumine.mythic.apiexamples.drops.ExampleItem;
+import io.lumine.mythic.apiexamples.mechanics.ExampleMechanic;
+import io.lumine.mythic.bukkit.events.MythicConditionLoadEvent;
+import io.lumine.mythic.bukkit.events.MythicDropLoadEvent;
 import io.lumine.mythic.bukkit.events.MythicMechanicLoadEvent;
 import me.deecaad.weaponmechanics.WeaponMechanics;
 import org.bukkit.event.EventHandler;
@@ -15,6 +20,20 @@ public class MythicMobsLoader implements Listener {
     public void onLoad(MythicMechanicLoadEvent event) {
         if (event.getMechanicName().equalsIgnoreCase("weaponMechanicsShoot")) {
             event.register(new MythicMobsWeaponShootSkill(event.getConfig()));
+        }
+    }
+
+    @EventHandler
+    public void onMythicConditionLoad(MythicConditionLoadEvent event)	{
+        if (event.getConditionName().equalsIgnoreCase("weaponMechanicsArmed"))	{
+            event.register(new MythicMobsArmedCondition(event.getConfig()));
+        }
+    }
+
+    @EventHandler
+    public void onMythicDropLoad(MythicDropLoadEvent event)	{
+        if (event.getDropName().equalsIgnoreCase("weaponMechanicsWeapon"))	{
+            event.register(new MythicMobsWeaponDrop(event.getConfig(), event.getArgument()));
         }
     }
 }
