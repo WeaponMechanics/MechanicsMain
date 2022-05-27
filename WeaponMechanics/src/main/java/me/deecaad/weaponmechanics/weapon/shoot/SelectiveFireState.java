71d4642052f9b41d1ef0a5a30da5b5f04850639e
Collin <collinjbarber@gmail.com>
2022-05-26 21:15:54 -0400

    Add WeaponSelectiveFireChangeEvent #94

--- WeaponMechanics/src/main/java/me/deecaad/weaponmechanics/weapon/shoot/SelectiveFireState.java
+++ WeaponMechanics/src/main/java/me/deecaad/weaponmechanics/weapon/shoot/SelectiveFireState.java
@@ -1,5 +1,11 @@
 package me.deecaad.weaponmechanics.weapon.shoot;
 
+import me.deecaad.weaponmechanics.utils.CustomTag;
+import me.deecaad.weaponmechanics.weapon.weaponevents.WeaponSelectiveFireChangeEvent;
+import me.deecaad.weaponmechanics.wrappers.EntityWrapper;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.ItemStack;
+
 public enum SelectiveFireState {
 
     SINGLE(0),
@@ -14,5 +20,20 @@ public enum SelectiveFireState {
 
     public int getId() {
         return id;
+    }
+
+
+    /**
+     * Helper function to set item's selective fire state and call a
+     * {@link me.deecaad.weaponmechanics.weapon.weaponevents.WeaponSelectiveFireChangeEvent}.
+     */
+    public static void setState(EntityWrapper entity, String weaponTitle, ItemStack item, SelectiveFireState oldState, SelectiveFireState newState) {
+        WeaponSelectiveFireChangeEvent event = new WeaponSelectiveFireChangeEvent(weaponTitle, item, entity.getEntity(), oldState, newState);
+        Bukkit.getPluginManager().callEvent(event);
+
+        if (event.isCancelled())
+            return;
+
+        CustomTag.SELECTIVE_FIRE.setInteger(item, event.getNewState().id);
     }
 }
