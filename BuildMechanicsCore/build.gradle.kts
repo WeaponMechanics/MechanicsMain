0b9839f28f46fe3d747da604254252302ad9a43e
CJCrafter <collinjbarber@gmail.com>
2022-07-14 18:06:33 -0700

    Fixed color argument

--- BuildMechanicsCore/build.gradle.kts
+++ BuildMechanicsCore/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 description = "Library plugin for WeaponMechanics"
-version = "1.3.4-BETA"
+version = "1.3.5-BETA"
 
 plugins {
     `maven-publish`
