65edc8dae311e17a3653f380cd1199a9655ec1b0
DeeCaaD <perttu.kangas@hotmail.fi>
2022-08-29 17:26:19 +0300

    Fixed shooter being able to damage entity its currently riding

--- WeaponMechanics/src/main/java/me/deecaad/weaponmechanics/weapon/projectile/weaponprojectile/WeaponProjectile.java
+++ WeaponMechanics/src/main/java/me/deecaad/weaponmechanics/weapon/projectile/weaponprojectile/WeaponProjectile.java
@@ -64,7 +64,8 @@ public class WeaponProjectile extends AProjectile {
                     .withBlockFilter(this::equalToLastHit)
                     .withEntityFilter(entity ->
                             equalToLastHit(entity)
-                            || (getShooter() != null && getAliveTicks() < 10 && entity.getEntityId() == getShooter().getEntityId()))
+                                    || (getShooter() != null && getAliveTicks() < 10 && entity.getEntityId() == getShooter().getEntityId())
+                                    || (entity.getPassengers().contains(getShooter())))
                     .enableLiquidChecks()
                     .withRaySize(projectileSettings.getSize());
         }
