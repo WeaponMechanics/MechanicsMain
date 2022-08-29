df1c3577e0b681739751801503e4eab140819ce7
DeeCaaD <perttu.kangas@hotmail.fi>
2022-08-29 17:25:58 +0300

    Fixed shooter being dismounted in 1.17+

--- WeaponCompatibility/Weapon_1_17_R1/src/main/java/me/deecaad/weaponmechanics/compatibility/v1_17_R1.java
+++ WeaponCompatibility/Weapon_1_17_R1/src/main/java/me/deecaad/weaponmechanics/compatibility/v1_17_R1.java
@@ -64,7 +64,7 @@ public class v1_17_R1 implements IWeaponCompatibility {
     @Override
     public void modifyCameraRotation(Player player, float yaw, float pitch, boolean absolute) {
         pitch *= -1;
-        ((CraftPlayer) player).getHandle().connection.send(new ClientboundPlayerPositionPacket(0, 0, 0, yaw, pitch, absolute ? ABSOLUTE_FLAGS : RELATIVE_FLAGS, 0, true));
+        ((CraftPlayer) player).getHandle().connection.send(new ClientboundPlayerPositionPacket(0, 0, 0, yaw, pitch, absolute ? ABSOLUTE_FLAGS : RELATIVE_FLAGS, 0, false));
     }
 
     @Override
