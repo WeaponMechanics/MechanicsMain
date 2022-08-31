9fce2c990666b39c5340ff57b5c5f85ea107bb4e
Collin <collinjbarber@gmail.com>
2022-08-31 00:07:37 -0400

    Added #100 - smooth damage dropoff

--- WeaponMechanics/src/main/java/me/deecaad/weaponmechanics/weapon/damage/DamageDropoff.java
+++ WeaponMechanics/src/main/java/me/deecaad/weaponmechanics/weapon/damage/DamageDropoff.java
@@ -3,6 +3,8 @@ package me.deecaad.weaponmechanics.weapon.damage;
 import me.deecaad.core.file.SerializeData;
 import me.deecaad.core.file.Serializer;
 import me.deecaad.core.file.SerializerException;
+import me.deecaad.core.utils.NumberUtil;
+import me.deecaad.weaponmechanics.WeaponMechanics;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -31,8 +33,23 @@ public class DamageDropoff implements Serializer<DamageDropoff> {
      * @return Damage modifier
      */
     public double getDamage(double distance) {
-        Map.Entry<Double, Double> floor = distances.floorEntry(distance);
+        if (WeaponMechanics.getBasicConfigurations().getBool("Smooth_Damage_Dropoff", false)) {
+            Map.Entry<Double, Double> floor = distances.floorEntry(distance);
+            Map.Entry<Double, Double> ceiling = distances.ceilingEntry(distance);
 
+            if (ceiling == null)
+                return floor == null ? 0.0 : floor.getValue();
+
+            // "Smooth Dropoff" is just a linear dropoff between the 2 points.
+            // Using inverse-lerp and lerp like this is called a remap
+            // function. 't' is a percentage of how far between 'distance' is
+            // between 'floor' and 'ceiling'. Then we just use that percentage
+            // to interpolate.
+            double t = NumberUtil.invLerp(floor == null ? 0.0 : floor.getKey(), ceiling.getKey(), distance);
+            return NumberUtil.lerp(floor == null ? 0.0 : floor.getValue(), ceiling.getValue(), t);
+        }
+
+        Map.Entry<Double, Double> floor = distances.floorEntry(distance);
         return floor == null ? 0.0 : floor.getValue();
     }
 
