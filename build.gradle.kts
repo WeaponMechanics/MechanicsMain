1b284f19b93992baf055ca76bdd5b06a8084f31f
DeeCaaD <perttu.kangas@hotmail.fi>
2022-09-06 22:17:16 +0300

    Add createGithubRelease task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,37 @@
+import com.github.breadmoirai.githubreleaseplugin.GithubReleaseTask
+
+plugins {
+    id("com.github.breadmoirai.github-release") version "2.4.1"
+}
+
+tasks.register<GithubReleaseTask>("createGithubRelease").configure {
+    // https://github.com/BreadMoirai/github-release-gradle-plugin
+    val weaponMechanicsVersion = project(":BuildWeaponMechanics").version.toString()
+
+    owner.set("WeaponMechanics")
+    repo.set("MechanicsMain")
+    authorization.set("Token ${findProperty("pass").toString()}")
+    tagName.set("v${weaponMechanicsVersion}")
+    targetCommitish.set("master")
+    releaseName.set("v${weaponMechanicsVersion} BETA")
+    draft.set(true)
+    prerelease.set(false)
+    generateReleaseNotes.set(true)
+    body.set("")
+    overwrite.set(false)
+    allowUploadToExisting.set(false)
+    apiEndpoint.set("https://api.github.com")
+
+    setReleaseAssets(file("build").listFiles())
+
+    // If set to true, you can debug that this would do
+    dryRun.set(false)
+
+    doFirst {
+        println("Creating GitHub release")
+    }
+}
+
 // This is a helper method to compile MechanicsCore, WeaponMechanics, and to
 // put all the stuff in zip files and such for github release.
 tasks.register("buildForSpigotRelease").configure {
@@ -15,27 +49,33 @@ tasks.register("buildForSpigotRelease").configure {
     file.appendText("WeaponMechanics: $weaponMechanicsVersion\n")
     file.appendText("WeaponMechanicsResourcePack: 1.1.0\n")
 
-    println("Compile MechanicsCore")
     dependsOn(":BuildMechanicsCore:shadowJar")
-    println("Compile WeaponMechanics")
     dependsOn(":BuildWeaponMechanics:shadowJar")
 
-    finalizedBy("zipForSpigotRelease", "resourcePackForSpigotRelease")
+    finalizedBy("resourcePackForSpigotRelease", "zipForSpigotRelease")
 }
 
 tasks.register<Copy>("resourcePackForSpigotRelease") {
-    println("Copy resource pack")
+    dependsOn("buildForSpigotRelease")
     val resourcePackVersion = "1.1.0"
     from("${layout.projectDirectory}\\resourcepack\\WeaponMechanicsResourcePack-${resourcePackVersion}.zip")
     into(layout.buildDirectory)
+
+    doFirst {
+        println("Copy resource pack")
+    }
 }
 
 tasks.register<Zip>("zipForSpigotRelease") {
-    println("Generate zip file")
+    dependsOn("buildForSpigotRelease", "resourcePackForSpigotRelease")
     archiveFileName.set("WeaponMechanics.zip")
     destinationDirectory.set(layout.buildDirectory)
 
     from (layout.buildDirectory) {
         include("*.jar")
+    }
+
+    doFirst {
+        println("Generate zip file")
     }
 }
