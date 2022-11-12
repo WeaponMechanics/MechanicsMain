11f93732aa22608cfac5cd40e1078ea2a7117950
DeeCaaD <perttu.kangas@hotmail.fi>
2022-11-12 20:40:11 +0200

    Update version

--- BuildWeaponMechanics/build.gradle.kts
+++ BuildWeaponMechanics/build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 description = "A New Age of Weapons in Minecraft"
-version = "1.13.4"
+version = "1.13.5"
 
 plugins {
     id("me.deecaad.java-conventions")
