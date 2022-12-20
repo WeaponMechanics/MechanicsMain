3b7910423af37dd2bb874e8e0418c37f4018033f
CJCrafter <collinjbarber@gmail.com>
2022-12-20 12:21:47 -0800

    Fix #211 - WeaponConverter getting replaced and not functioning

--- BuildWeaponMechanics/build.gradle.kts
+++ BuildWeaponMechanics/build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 
 description = "A New Age of Weapons in Minecraft"
-version = "1.14.8"
+version = "1.14.9"
 
 plugins {
     id("me.deecaad.java-conventions")
