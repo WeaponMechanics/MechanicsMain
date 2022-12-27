090b2ce1201fd9a92b145e5678c3e733e248617b
CJCrafter <collinjbarber@gmail.com>
2022-12-26 17:41:41 -0800

    Add crossbow item check

--- WeaponMechanics/src/main/java/me/deecaad/weaponmechanics/weapon/info/WeaponItemSerializer.java
+++ WeaponMechanics/src/main/java/me/deecaad/weaponmechanics/weapon/info/WeaponItemSerializer.java
@@ -33,6 +33,15 @@ public class WeaponItemSerializer extends ItemSerializer {
     public ItemStack serialize(SerializeData data) throws SerializerException {
         ItemStack weaponStack = super.serializeWithoutRecipe(data);
 
+        // Crossbows are not allowed to be used as the weapon's type. WMC
+        // supports "faking" the crossbow for other players. I added this check
+        // since it is a commonly asked question, and this should save me some
+        // time in the long run.
+        if (weaponStack.getType().name().equals("CROSSBOW"))
+            throw data.exception("Type", "You cannot use 'CROSSBOW' as a WeaponMechanics weapon!",
+                    "YES! We know that you want weapons to be 'held up' like a minecraft crossbow",
+                    "Purchase WMC to 'fake' the crossbow animation for other players: https://www.spigotmc.org/resources/104539/");
+
         String weaponTitle = data.key.split("\\.")[0];
         WeaponMechanics.getWeaponHandler().getInfoHandler().addWeapon(weaponTitle);
 
