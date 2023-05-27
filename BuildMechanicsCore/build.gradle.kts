c53feca96a2cbb5c703bbd4fc7218ea3b0a72fac
Collin <collinjbarber@gmail.com>
2023-05-27 12:33:00 -0400

    Fix inline item support for repair kits, and ammo

--- BuildMechanicsCore/build.gradle.kts
+++ BuildMechanicsCore/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 description = "Library plugin for WeaponMechanics"
-version = "2.2.4"
+version = "2.2.5"
 
 plugins {
     `maven-publish`
