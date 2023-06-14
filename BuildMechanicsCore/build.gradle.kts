4cdd7546bb9213340b0a80cb5912224f99307a42
Collin <collinjbarber@gmail.com>
2023-06-14 12:02:01 -0400

    increment version for publish

--- BuildMechanicsCore/build.gradle.kts
+++ BuildMechanicsCore/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 description = "Library plugin for WeaponMechanics"
-version = "2.4.0"
+version = "2.4.1"
 
 plugins {
     `maven-publish`
