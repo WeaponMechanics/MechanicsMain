efa29d319e2fae7c8ba17239e422488399c577d3
Collin <collinjbarber@gmail.com>
2023-08-15 00:03:53 -0400

    add lightning mechanic

---
+++ MechanicsCore/src/main/java/me/deecaad/core/mechanics/defaultmechanics/LightningMechanic.java
@@ -0,0 +1,48 @@
+package me.deecaad.core.mechanics.defaultmechanics;
+
+import me.deecaad.core.file.SerializeData;
+import me.deecaad.core.file.SerializerException;
+import me.deecaad.core.mechanics.CastData;
+import org.bukkit.Location;
+import org.jetbrains.annotations.NotNull;
+
+public class LightningMechanic extends Mechanic {
+
+    private boolean isEffect;
+
+    /**
+     * Default constructor for serializer
+     */
+    public LightningMechanic() {
+    }
+
+    public LightningMechanic(boolean isEffect) {
+        this.isEffect = isEffect;
+    }
+
+    public boolean isEffect() {
+        return isEffect;
+    }
+
+    @Override
+    public String getKeyword() {
+        return "Lightning";
+    }
+
+    @Override
+    protected void use0(CastData cast) {
+        Location strikeLocation = cast.getTargetLocation();
+        if (isEffect)
+            strikeLocation.getWorld().strikeLightningEffect(strikeLocation);
+        else
+            strikeLocation.getWorld().strikeLightning(strikeLocation);
+    }
+
+    @NotNull
+    @Override
+    public Mechanic serialize(SerializeData data) throws SerializerException {
+        boolean isEffect = data.of("Effect").getBool(false);
+
+        return applyParentArgs(data, new LightningMechanic(isEffect));
+    }
+}
