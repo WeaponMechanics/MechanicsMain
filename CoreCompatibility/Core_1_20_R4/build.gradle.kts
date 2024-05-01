b1dfb8371ffb02b45b23420f849f2a8a9a1a48d4
Collin Barber <collinjbarber@gmail.com>
2024-05-01 18:51:49 -0400

    add R4 files

---
+++ CoreCompatibility/Core_1_20_R4/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("me.deecaad.mechanics-project")
+    id("io.papermc.paperweight.userdev")
+}
+
+dependencies {
+    compileOnly(project(":MechanicsCore"))
+    adventureChatAPI()
+
+    paperweight.paperDevBundle("1.20.4-R0.1-SNAPSHOT")
+}
+
+tasks {
+    compileJava {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+        options.release.set(21)
+    }
+}
