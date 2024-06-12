6457fbc90bd085997d63620e828e2ccad14ac1cd
Collin Barber <collinjbarber@gmail.com>
2024-06-12 16:42:21 -0500

    deprecate TaskChain

--- MechanicsCore/src/main/java/me/deecaad/core/file/TaskChain.java
+++ MechanicsCore/src/main/java/me/deecaad/core/file/TaskChain.java
@@ -7,6 +7,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.NoSuchElementException;
 import java.util.function.Function;
 
+@Deprecated
 public class TaskChain {
 
     // * ----- TASK CLASS ----- * //
