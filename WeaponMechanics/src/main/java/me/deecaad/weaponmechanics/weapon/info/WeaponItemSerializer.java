cd00c18b71a9f5443b7a1aa98e3b3f61e1be2914
Collin Barber <collinjbarber@gmail.com>
2024-08-27 23:32:17 -0400

    don't require item name/lore, just skip (#454)
    * don't require item name/lore, just skip
    
    * put curly braces there, you psychopath

--- WeaponMechanics/src/main/java/me/deecaad/weaponmechanics/weapon/info/WeaponItemSerializer.java
+++ WeaponMechanics/src/main/java/me/deecaad/weaponmechanics/weapon/info/WeaponItemSerializer.java
@@ -39,21 +39,26 @@ public class WeaponItemSerializer extends ItemSerializer {
         // supports "faking" the crossbow for other players. I added this check
         // since it is a commonly asked question, and this should save me some
         // time in the long run.
-        if (weaponStack.getType().name().equals("CROSSBOW"))
+        if (weaponStack.getType().name().equals("CROSSBOW")) {
             throw data.exception("Type", "You cannot use 'CROSSBOW' as a WeaponMechanics weapon!",
                 "YES! We know that you want weapons to be 'held up' like a minecraft crossbow",
                 "Purchase WMC to 'fake' the crossbow animation for other players: https://www.spigotmc.org/resources/104539/");
+        }
 
         String weaponTitle = data.key.split("\\.")[0];
 
         // Saving display name and lore for use in WeaponMechanicsPlus to auto update items
         Configuration config = WeaponMechanics.getConfigurations();
-        config.set(weaponTitle + ".Info.Weapon_Item.Name", "<!italic>" + data.of("Name").assertExists().getAdventure());
-        List<String> unparsedLore = ((List<String>) data.of("Lore").assertExists().get())
-            .stream()
-            .map(line -> "<!italic>" + StringUtil.colorAdventure(line))
-            .toList();
-        config.set(weaponTitle + ".Info.Weapon_Item.Lore", unparsedLore);
+        String weaponDisplay = data.of("Name").getAdventure(null);
+        if (weaponDisplay != null) {
+            config.set(weaponTitle + ".Info.Weapon_Item.Display", weaponDisplay);
+        }
+        List<String> weaponLore = data.of("Lore").get(null);
+        if (weaponLore != null) {
+            config.set(weaponTitle + ".Info.Weapon_Item.Lore", weaponLore.stream()
+                .map(line -> "<!italic>" + StringUtil.colorAdventure(line))
+                .toList());
+        }
 
         // Ensure the weapon title uses the correct format, mostly for other plugin compatibility
         Pattern pattern = Pattern.compile("[A-Za-z0-9_]+");
