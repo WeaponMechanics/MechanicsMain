931713da8729a5e7cc1a73f9947190824c51b985
Collin Barber <collinjbarber@gmail.com>
2024-09-23 22:30:40 -0400

    fix javadoc error for duplicate key errors

--- MechanicsCore/src/main/java/me/deecaad/core/file/DuplicateKeyException.java
+++ MechanicsCore/src/main/java/me/deecaad/core/file/DuplicateKeyException.java
@@ -1,16 +1,11 @@
 package me.deecaad.core.file;
 
-import org.bukkit.configuration.ConfigurationSection;
-
 import java.util.Arrays;
 import java.util.Collection;
 
 /**
  * This class outlines an {@link Exception} that occurs when 2 configuration files with a matching
- * key are added to the same {@link ConfigurationOld}
- *
- * @see ConfigurationOld#add(ConfigurationOld)
- * @see ConfigurationOld#add(ConfigurationSection)
+ * key are added to the same {@link Configuration}
  */
 public class DuplicateKeyException extends Exception {
 
