db4d7b953471e04b8b5a1e47015ad0fdb5f6e513
Collin Barber <collinjbarber@gmail.com>
2025-01-07 21:46:14 -0500

    update adventureChatAPI

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -10,8 +10,8 @@ object Versions {
     const val VIVECRAFT = "3.0.0"
 
     // Adventure Chat API
-    const val ADVENTURE_API = "4.17.0"
-    const val ADVENTURE_BUKKIT = "4.3.3"
+    const val ADVENTURE_API = "4.18.0"
+    const val ADVENTURE_BUKKIT = "4.3.4"
     const val ADVENTURE_TEXT_LEGACY = ADVENTURE_API
     const val ADVENTURE_TEXT_MINIMESSAGE = ADVENTURE_API
 
